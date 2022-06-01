c68e6a12b322ea12f4eeb71437129a93753acf9e
whumhr <whumhr@foxmail.com>
2022-06-01 07:01:28 +0000

    update controller/relation.go.

--- controller/relation.go
+++ controller/relation.go
@@ -1,13 +1,17 @@
 package controller
 
 import (
+	"github.com/RaymondCode/simple-demo/constants"
+	"github.com/RaymondCode/simple-demo/structs"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"gorm.io/driver/mysql"
 	"net/http"
 )
 
 type UserListResponse struct {
-	Response
-	UserList []User `json:"user_list"`
+	structs.Response
+	UserList []structs.User `json:"user_list"`
 }
 
 // RelationAction no practical effect, just check if token is valid
@@ -15,33 +19,33 @@ func RelationAction(c *gin.Context) {
 	token := c.Query("token")
 
 	if _, exist := usersLoginInfo[token]; exist {
-		c.JSON(http.StatusOK, Response{StatusCode: 0})
+		c.JSON(http.StatusOK, structs.Response{StatusCode: 0})
 	} else {
-		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
+		c.JSON(http.StatusOK, structs.Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
 	}
 }
 
 // FollowList all users have same follow list
 func FollowList(c *gin.Context) {
 	c.JSON(http.StatusOK, UserListResponse{
-		Response: Response{
+		Response: structs.Response{
 			StatusCode: 0,
 		},
-		UserList: []User{DemoUser},
+		UserList: []structs.User{DemoUser},
 	})
 }
 
 // FollowerList all users have same follower list
 func FollowerList(c *gin.Context) {
 	//连接数据库
-	db, _ := gorm.Open(mysql.Open(constants.MySQLDefaultDSN))
+	db, err := gorm.Open(mysql.Open(constants.MySQLDefaultDSN))
 	var users []User
 	err = db.Select("id", "name").Find(&users, 1).Error
 
 	c.JSON(http.StatusOK, UserListResponse{
-		Response: Response{
+		Response: structs.Response{
 			StatusCode: 0,
 		},
-		UserList: []User{DemoUser},
+		UserList: []structs.User{DemoUser},
 	})
 }
